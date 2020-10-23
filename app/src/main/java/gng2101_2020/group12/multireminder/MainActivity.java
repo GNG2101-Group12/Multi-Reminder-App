9f5eecbb69684afd884e1ac25966fb5ed67207ff
FarinaSalman <farinasalman@gmail.com>
2020-10-23 15:34:31 -0400

    added create a reminder layout

--- app/src/main/java/gng2101_2020/group12/multireminder/MainActivity.java
+++ app/src/main/java/gng2101_2020/group12/multireminder/MainActivity.java
@@ -3,6 +3,8 @@ package gng2101_2020.group12.multireminder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
