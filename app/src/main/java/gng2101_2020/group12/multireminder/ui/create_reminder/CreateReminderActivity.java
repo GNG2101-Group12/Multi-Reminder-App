763f77f035dfa6db31c3a737efd1f4b25d95533f
Ajay Ramachandran <dev@ajay.app>
2020-11-01 12:50:00 -0500

    Add time pickers to reminder creation

--- app/src/main/java/gng2101_2020/group12/multireminder/ui/create_reminder/CreateReminderActivity.java
+++ app/src/main/java/gng2101_2020/group12/multireminder/ui/create_reminder/CreateReminderActivity.java
@@ -1,7 +1,10 @@
 package gng2101_2020.group12.multireminder.ui.create_reminder;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -10,8 +13,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.Calendar;
+import java.util.function.Function;
+
+import gng2101_2020.group12.multireminder.Helpers;
 import gng2101_2020.group12.multireminder.R;
 import gng2101_2020.group12.multireminder.ui.main.SectionsPagerAdapter;
+import mobi.upod.timedurationpicker.TimeDurationPicker;
+import mobi.upod.timedurationpicker.TimeDurationPickerDialog;
 
 public class CreateReminderActivity extends AppCompatActivity {
 
@@ -19,5 +28,39 @@ public class CreateReminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_reminder);
+
+        EditText chooseTime = findViewById(R.id.choose_time);
+        chooseTime.setOnClickListener((view) -> openTimePicker(view));
+        chooseTime.setOnFocusChangeListener((view, b) -> {if (b) openTimePicker(view);});
+
+        EditText snoozeDuration = findViewById(R.id.snoozeDuration);
+        //TODO: Fix this to actually be minutes
+        snoozeDuration.setOnClickListener((view) -> openDurationPicker(view));
+        snoozeDuration.setOnFocusChangeListener((view, b) -> {if (b) openDurationPicker(view);});
+    }
+
+    public void openTimePicker(View view) {
+        Calendar mcurrentTime = Calendar.getInstance();
+        TimePickerDialog timePickerDialog = new TimePickerDialog(CreateReminderActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay,
+                                          int minute) {
+                        ((EditText) view).setText(Helpers.formatDate(hourOfDay, minute));
+                    }
+                }, mcurrentTime.get(Calendar.HOUR_OF_DAY),
+                mcurrentTime.get(Calendar.MINUTE), true);
+        timePickerDialog.show();
+    }
+
+    public void openDurationPicker(View view) {
+        TimeDurationPickerDialog timePickerDialog = new TimeDurationPickerDialog(CreateReminderActivity.this,
+                new TimeDurationPickerDialog.OnDurationSetListener() {
+                    @Override
+                    public void onDurationSet(TimeDurationPicker v, long duration) {
+                        ((EditText) view).setText(Helpers.formatDuration(duration));
+                    }
+                }, 600000);
+        timePickerDialog.show();
     }
 }
